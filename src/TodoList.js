d7967780ef1dd443c90529a3c27b4fcdb6987f35
NishimuraTakuya-nt <takuya.nishimura.chel@gmail.com>
2024-09-21 14:02:32 +0900

    init todo list

---
+++ src/TodoList.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import Todo from "./Todo";
+
+const TodoList = ({ todos, toggleTodo }) => {
+    return todos.map((todo) => <Todo todo={todo} key={todo.id} toggleTodo={toggleTodo} />)
+};
+
+export default TodoList;
